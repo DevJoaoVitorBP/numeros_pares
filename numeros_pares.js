a8a438d5e3fb0581a52f6c5f661ed491f90f8d8a
João Vitor <76665184+Joao-Pereira013@users.noreply.github.com>
2023-08-05 15:28:01 -0300

    Update numeros_pares.js

--- numeros_pares.js
+++ numeros_pares.js
@@ -1,9 +1,9 @@
 //Criando um programa que percorra uma lista e pegue apenas números pares
 
-const numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numerosPares = [];
 
-for (let i = 0; i < numero.length; i++) {
-    if (numero[i] % 2 == 0) {
-        console.log(numero[i]);
+    for (let i = 0; i < 10; i = i + 2) {
+        numerosPares.push(i);
     }
-}
+
+    console.log(numerosPares);
