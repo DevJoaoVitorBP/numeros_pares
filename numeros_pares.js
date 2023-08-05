b72198042532c5797eec055da565f7d18c9de4ca
João Vitor <76665184+Joao-Pereira013@users.noreply.github.com>
2023-08-05 15:34:31 -0300

    Update numeros_pares.js

--- numeros_pares.js
+++ numeros_pares.js
@@ -1,9 +1,21 @@
 //Criando um programa que percorra uma lista e pegue apenas números pares
 
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] % 2 == 0) {
+        console.log(numeros[i]);
+    }
+}
+
+/* Imprimindo uma lista com 5 primeiros números pares.
+
 const numerosPares = [];
 
-    for (let i = 0; i < 10; i = i + 2) {
-        numerosPares.push(i);
-    }
+for (let i = 0; i < 10; i = i + 2) {
+    numerosPares.push(i);
+}
 
-    console.log(numerosPares);
+console.log(numerosPares);
+
+*/
